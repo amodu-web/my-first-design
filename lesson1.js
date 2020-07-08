ef2789ccf6c753dfe877a015ad0863e3c444057d
amodu-web <amoduisaac777@gmail.com>
2020-07-08 18:38:05 +0100

    using js to solve maths

---
+++ lesson1.js
@@ -0,0 +1,41 @@
+// variable declarations
+// const, let, var
+//addition
+// let a = 2;
+// let b = 10;
+// const c = a + b
+// console.log(c + 2)
+
+// //substration
+// var d = 5
+// var e = 3
+// var f = d - e
+// console.log("The result of F is: " + f)
+
+// var arr = [1,2,3]
+// console.log(arr)
+//datatypes
+// Number e.g 1,2,3....
+// String e.g name, house, dog.....
+// Array e.g [1,2,3,...]
+// Boolean e.g true or false, 0 or 1
+// Integer e.g 1,1.1,
+// decimal e.g 3.142
+// float e.g 12.5f
+
+// area of a circle = pie*r^2, take r=7
+let r=7
+let pie=3.124
+let area = pie*r*r
+console.log(area)
+//area of a rectangle
+// -b+root(d)\2a or -b-root(d)/2a where d = b^2 - 4ac
+let a =2
+let b =8
+let c =2
+let d = b*b - (4*a*c)
+let eqn1 = -b+Math.sqrt(d)/2*a
+let eqn2=-b-Math.sqrt(d)/2*a
+console.log(eqn1)
+console.log(eqn2)
+
